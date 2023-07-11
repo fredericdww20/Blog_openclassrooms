31f62ab36cd36aab0a0f5f8797323dfbe9e15caf
Frederic Portemer <synapsefred@hotmail.fr>
2023-07-11 13:10:22 +0200

    Update reformat code

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -2,110 +2,110 @@
 /**
  *
  */
+
 namespace App\Controllers;
 
 use App\Models\AdminManager;
 
 
-
 class AdminController extends Controller
 {
-	public function listcomment(): string
-	{
-		$adminManager = new AdminManager();
-		$comments = $adminManager->fetchcomment();
-		$message = $_SESSION['message'];
-		$output = $this->twig->render('admin/comment.html.twig', [
-			'comments' => $comments,
-			'message' => $message
-		]);
-		unset($_SESSION['message']);
-		return $output;
-	}
+    public function listcomment(): string
+    {
+        $adminManager = new AdminManager();
+        $comments = $adminManager->fetchcomment();
+        $message = $_SESSION['message'];
+        $output = $this->twig->render('admin/comment.html.twig', [
+            'comments' => $comments,
+            'message' => $message
+        ]);
+        unset($_SESSION['message']);
+        return $output;
+    }
 
-	public function listvalidate(): string
-	{
-		$adminManager = new AdminManager();
-		$posts = $adminManager->fetchAll();
-		return $this->twig->render('admin/admin.html.twig', [
-			'posts' => $posts]);
-	}
+    public function listvalidate(): string
+    {
+        $adminManager = new AdminManager();
+        $posts = $adminManager->fetchAll();
+        return $this->twig->render('admin/admin.html.twig', [
+            'posts' => $posts]);
+    }
 
-	public function list(): string
-	{
-		$adminManager = new AdminManager();
-		$posts = $adminManager->fetchvalidate();
-		$message = $_SESSION['message'];
-		$output = $this->twig->render('admin/posts.html.twig', [
-			'posts' => $posts,
-			'message' => $message
-		]);
-		unset($_SESSION['message']);
-		return $output;
-	}
+    public function list(): string
+    {
+        $adminManager = new AdminManager();
+        $posts = $adminManager->fetchvalidate();
+        $message = $_SESSION['message'];
+        $output = $this->twig->render('admin/posts.html.twig', [
+            'posts' => $posts,
+            'message' => $message
+        ]);
+        unset($_SESSION['message']);
+        return $output;
+    }
 
-	public function update($id)
-	{
-		$adminManager = new AdminManager();
-		$message = null;
-		$errors = [];
-		$sta = null;
+    public function update($id)
+    {
+        $adminManager = new AdminManager();
+        $message = null;
+        $errors = [];
+        $sta = null;
 
-		if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-			if (isset($_POST['sta'])) {
-				$sta = htmlspecialchars($_POST['sta'], ENT_QUOTES, 'UTF-8');
-				if (empty($sta)) {
-					$errors[] = 'Le champ "sta" est requis.';
-				}
-				if (empty($errors)) {
-					$adminManager->update($id, $sta);
-					$_SESSION['message'] = 'Mise à jour réussie';
-					header('Location: /OpenClassrooms/admin/posts');
-					return; // Renvoie un message de succès si la mise à jour à reussie
-				}
-			}
-		}
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['sta'])) {
+                $sta = htmlspecialchars($_POST['sta'], ENT_QUOTES, 'UTF-8');
+                if (empty($sta)) {
+                    $errors[] = 'Le champ "sta" est requis.';
+                }
+                if (empty($errors)) {
+                    $adminManager->update($id, $sta);
+                    $_SESSION['message'] = 'Mise à jour réussie';
+                    header('Location: /OpenClassrooms/admin/posts');
+                    return; // Renvoie un message de succès si la mise à jour à reussie
+                }
+            }
+        }
 
-		return $this->twig->render('admin/edit.html.twig', [
-			'id' => $id,
-			'sta' => $sta,
-			'message' => $message,
-			'errors' => $errors,
-		]);
-	}
+        return $this->twig->render('admin/edit.html.twig', [
+            'id' => $id,
+            'sta' => $sta,
+            'message' => $message,
+            'errors' => $errors,
+        ]);
+    }
 
-	public function updatecomments($id)
-	{
-		$adminManager = new AdminManager();
-		$message = null;
-		$errors = [];
-		$sta = null;
+    public function updatecomments($id)
+    {
+        $adminManager = new AdminManager();
+        $message = null;
+        $errors = [];
+        $sta = null;
 
-		if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-			if (isset($_POST['sta'])) {
-				$sta = htmlspecialchars($_POST['sta'], ENT_QUOTES, 'UTF-8');
-				if (empty($sta)) {
-					$errors[] = 'Le champ "sta" est requis.';
-				}
-				if (empty($errors)) {
-					try {
-						$adminManager->updatecomment($id, $sta);
-						$_SESSION['message'] = 'Mise à jour réussie';
-						header('Location: /OpenClassrooms/admin/comment');
-						return; // Renvoie un message de succès si la mise à jour à reussie
-					} catch (Exception $e) {
-						// Gérer l'exception ou envoie l'erreur
-						$errors[] = 'Une erreur s\'est produite lors de la mise à jour du commentaire.';
-					}
-				}
-			}
-		}
-		return $this->twig->render('admin/editco mment.html.twig', [
-			'id' => $id,
-			'sta' => $sta,
-			'message' => $message,
-			'errors' => $errors,
-		]);
-	}
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['sta'])) {
+                $sta = htmlspecialchars($_POST['sta'], ENT_QUOTES, 'UTF-8');
+                if (empty($sta)) {
+                    $errors[] = 'Le champ "sta" est requis.';
+                }
+                if (empty($errors)) {
+                    try {
+                        $adminManager->updatecomment($id, $sta);
+                        $_SESSION['message'] = 'Mise à jour réussie';
+                        header('Location: /OpenClassrooms/admin/comment');
+                        return; // Renvoie un message de succès si la mise à jour à reussie
+                    } catch (Exception $e) {
+                        // Gérer l'exception ou envoie l'erreur
+                        $errors[] = 'Une erreur s\'est produite lors de la mise à jour du commentaire.';
+                    }
+                }
+            }
+        }
+        return $this->twig->render('admin/editco mment.html.twig', [
+            'id' => $id,
+            'sta' => $sta,
+            'message' => $message,
+            'errors' => $errors,
+        ]);
+    }
 
 }
--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -7,61 +7,63 @@ use App\Models\PostManager;
 use PDOException;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
+
 /**
  * Class CommentController
  */
 class CommentController extends Controller
 {
-	/**
-	 * @return string
-	 * @throws RuntimeError
-	 * @throws SyntaxError
-	 * @throws \Twig\Error\LoaderError
-	 */
-	public function addcomment(): string
-	{
-		$message = null;
+    /**
+     * @return string
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws \Twig\Error\LoaderError
+     */
+    public function addcomment(): string
+    {
+        $message = null;
 
-		if (!empty($_POST['title']) && !empty($_POST['commentary']) && !empty($_POST['id_post'])) {
-			$commentManager = new CommentManager();
+        if (!empty($_POST['title']) && !empty($_POST['commentary']) && !empty($_POST['id_post'])) {
+            $commentManager = new CommentManager();
 
-			$postId = intval($_POST['id_post']);
+            $postId = intval($_POST['id_post']);
 
-			// Vérifiez si l'ID de l'utilisateur est défini dans $_SESSION
-			if (isset($_SESSION['user_id']) && is_int($_SESSION['user_id'])) {
-				$userId = $_SESSION['user_id'];
+            // Vérifiez si l'ID de l'utilisateur est défini dans $_SESSION
+            if (isset($_SESSION['user_id']) && is_int($_SESSION['user_id'])) {
+                $userId = $_SESSION['user_id'];
 
-				try {
-					$commentManager->commentate($_POST['title'], $_POST['commentary'], $postId, $userId);
-					$message = 'Commentaire envoyé';
-				} catch (PDOException $e) {
-					$message = 'Une erreur s\'est produite lors de l\'envoi du commentaire : ' . $e->getMessage();
-				}
-			}  {
-				$message = 'ID d\'utilisateur invalide.';
-			}
-		} else {
-			$message = 'Veuillez remplir tous les champs du formulaire.';
-		}
+                try {
+                    $commentManager->commentate($_POST['title'], $_POST['commentary'], $postId, $userId);
+                    $message = 'Commentaire envoyé';
+                } catch (PDOException $e) {
+                    $message = 'Une erreur s\'est produite lors de l\'envoi du commentaire : ' . $e->getMessage();
+                }
+            }
+            {
+                $message = 'ID d\'utilisateur invalide.';
+            }
+        } else {
+            $message = 'Veuillez remplir tous les champs du formulaire.';
+        }
 
-		$postManager = new PostManager();
-		$post = $postManager->fetch($postId);
+        $postManager = new PostManager();
+        $post = $postManager->fetch($postId);
 
-		return $this->twig->render('list/post.html.twig', [
-			'message' => $message,
-			'post' => $post,
-		]);
-	}
+        return $this->twig->render('list/post.html.twig', [
+            'message' => $message,
+            'post' => $post,
+        ]);
+    }
 
-	public function showComment($id)
-	{
-		$commentRepository = new CommentManager();
+    public function showComment($id)
+    {
+        $commentRepository = new CommentManager();
 
-		$comments = $commentRepository->fetch($id);
+        $comments = $commentRepository->fetch($id);
 
-		return $this->twig->render('list/post.html.twig', [
-			'comments' => $comments
-		]);
-	}
+        return $this->twig->render('list/post.html.twig', [
+            'comments' => $comments
+        ]);
+    }
 
 }
