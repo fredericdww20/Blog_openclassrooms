3bef8b0121768c5dc4c3cb979e5eab618dc0c17f
Frederic Portemer <synapsefred@hotmail.fr>
2023-07-18 08:45:17 +0200

    Update UserController.php

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -14,4 +14,28 @@ class UserController extends Controller
             'user' => $user,
         ]);
     }
+
+    public function add(): string
+    {
+
+        $message = null;
+
+        if (!empty ($_POST)) {
+
+            $image =  htmlentities($_POST['image']);
+
+            $userManager = new UserManager();
+
+            // Appel de la méthode addImage du UserManager
+            $userManager->addImage($_POST['image']);
+
+            // Afficher un message de succès ou effectuer d'autres opérations après l'ajout de l'image
+
+            $message = "L'image a été ajoutée avec succès.";
+        }
+
+        // Rendu du template avec le message (ou d'autres données) si nécessaire
+        return $this->twig->render('profil/profil.html.twig', ['message' => $message]);
+    }
+
 }
