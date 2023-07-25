7f0040938f1919f03d4e7e1a271d132a4b30255c
Frederic Portemer <yanolabteam@gmail.com>
2023-07-25 13:06:48 +0200

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -22,4 +22,31 @@ class Request
     {
         return $_SERVER['REQUEST_METHOD'] === 'POST';
     }
+
+    private function validateForm($formData): bool
+    {
+        return $this->validateFields($formData);
+    }
+
+    private function validateFields($formData): bool
+    {
+        $fields = ['firstname', 'lastname', 'email', 'password'];
+        foreach ($fields as $field) {
+            if (empty($formData[$field])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public function checkEmailExists(string $email): bool
+    {
+        $sql = 'SELECT COUNT(*) FROM user WHERE email = :email';
+
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute([
+            'email' => $email,
+        ]);
+        $count = $statement->fetchColumn();
+        return ($count > 0);
+    }
 }
