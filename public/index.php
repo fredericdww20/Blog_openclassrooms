c1dc4bbae9747b28aff790c0c05db35402d42511
Frederic Portemer <synapsefred@hotmail.fr>
2023-10-03 09:38:57 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
