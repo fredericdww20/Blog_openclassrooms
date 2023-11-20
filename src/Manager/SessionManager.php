bb7ff11162f2044632b8d73b8263872bacab73f0
Frederic Portemer <synapsefred@hotmail.fr>
2023-11-20 21:27:00 +0100

    Update redirect admin

---
+++ src/Manager/SessionManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Manager;
+
+class SessionManager
+{
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get(string $key): mixed
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function remove(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+
+}
